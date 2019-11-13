d1e1a6ae528f167b7ce68bf0f78b4047e24ce4cb
vishal <vishal@wagstays.com>
2019-11-13 19:07:11 +0530

    inital commmit

---
+++ arithmetic_operation/calculation.py
@@ -0,0 +1,9 @@
+
+
+
+def sum(*args):
+    sum = 0
+    print(args)
+    for val in args:
+        sum = sum + val
+    return sum
---
+++ running_result/result.py
@@ -0,0 +1,8 @@
+# import os
+import sys
+sys.path.append("..") # added to  make  top level  as base directory
+from arithmetic_operation.calculation import sum
+
+
+
+print(sum(1,2,3))
